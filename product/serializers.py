d2c723424987629ac347c986852c4bed81443ca2
Aldiiar <aldiarm27@gmail.com>
2023-04-06 16:38:46 +0600

    done 4hw

--- product/serializers.py
+++ product/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from product.models import Category, Product, Review
+from rest_framework.exceptions import ValidationError
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -32,3 +33,34 @@ class ProductsReviewsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = 'title reviews rating'.split()
+
+
+class ProductValidateSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=150)
+    description = serializers.CharField(required=False, default="No description")
+    price = serializers.FloatField()
+    category_id = serializers.IntegerField()
+
+    def validate_category_id(self, category_id):
+        try:
+            Category.objects.get(id=category_id)
+        except Category.DoesNotExist:
+            raise ValidationError('Category not found!')
+        return category_id
+
+
+class CategoryValidateSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=150, min_length=3)
+
+
+class ReviewValidateSerializer(serializers.Serializer):
+    text = serializers.CharField(max_length=500)
+    stars = serializers.IntegerField(required=False, min_value=1, max_value=5)
+    product_id = serializers.IntegerField()
+
+    def validate_product_id(self, product_id):
+        try:
+            Product.objects.get(id=product_id)
+        except Product.DoesNotExist:
+            raise ValidationError('Product not found!')
+        return product_id
